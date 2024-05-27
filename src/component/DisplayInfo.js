ffbb95e15faf1429dfa1f5f84bc69775d51b013e
DeiYui <www.doducduy@gmail.com>
2024-05-27 23:16:13 +0700

    suicide

--- src/component/DisplayInfo.js
+++ src/component/DisplayInfo.js
@@ -1,14 +1,39 @@
 import React from 'react'
 
 class DisplayInfo extends React.Component {
+
+    state = {
+        isShowListUser: true
+    }
+    handleShowHide = () => {
+        this.setState({
+            isShowListUser: !this.state.isShowListUser
+        })
+    }
     render () {
         //destructuring array/object
-        const { age, name } = this.props;
+        const { listUsers } = this.props;
         //props
         return(
             <div>
-                <div>My name is {name}</div>
-                <div>My age is {age}</div>
+                <div>
+                    <span onClick={() => {this.handleShowHide() }}> 
+                        {this.state.isShowListUser === true ? "Hide list users" : "Show list users"}
+                    </span>
+                </div>
+                {this.state.isShowListUser &&
+                    <div>
+                {listUsers.map((user) => {
+                     return (
+                        <div key={user.id} className={+user.age > 16 ? "green" : "red"}>
+                            <div>My name is {user.name}</div>
+                            <div>My age is {user.age}</div>
+                            <hr/>
+                        </div>
+                     )
+                })}
+                    </div>
+                }      
             </div>
         )
     }
--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -4,16 +4,24 @@ import DisplayInfo from './DisplayInfo';
 
 class MyComponent extends React.Component{
 
+    state = {
+        listUsers: [
+            {id: 1, name : "megumin", age: "14"},
+            {id: 2, name : "aqua", age: "17"},
+            {id: 3, name : "darkness", age: "18"},
+        ]
+    }
+
  
     //JSX
     render() {
-        const myInfo = ['a', 'b', 'c']
+
         return(
             <div>
                 <UserInfo></UserInfo>
                 <br/> <br/>
-                <DisplayInfo name="Aqua" age="18" />
-                <DisplayInfo name="Megumin" age={14} myInfo={myInfo} />
+                <DisplayInfo
+                listUsers={this.state.listUsers} />
 
             </div>
         );    
